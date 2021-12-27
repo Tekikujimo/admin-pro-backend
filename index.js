5b5fc8d64f78bf9a07fe7d1e94762952754f84dc
Diego Cavero <diegocavero2035@hotmail.com>
2021-12-27 19:30:32 +0100

    Fin seccion 10 - Auth

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ require('dotenv').config();
 
 const  { dbConnection } = require('./database/config')
 
+// Middleware son funciones que se ejecutan antes de otras
+
 // Crear el servidor de express
 const app = express();
 
 // Configurar CORS
 app.use(cors());
+
+// Lectura y parseo del body
+app.use(express.json());
 
 //Base de datos
 dbConnection();
@@ -20,14 +25,11 @@ dbConnection();
 
 
 // Rutas
-app.get('/', (req, res) => {
 
-    res.status(400).json({
-        ok: true,
-        msg: 'Hola Mundo'
-    })
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') );
 
-});
+
 
 app.listen( process.env.PORT, ()=>{
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
